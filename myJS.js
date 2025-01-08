34cea4b42d333cab463aa9348b14b622931cbe45
hekxo <raminsagandykov@gmail.com>
2025-01-08 10:23:34 +0500

    Initial Commit

--- myJS.js
+++ myJS.js
@@ -64,24 +64,6 @@ searchBtn1.addEventListener("click", () => {
 });
 
 
-let quote = document.getElementById("quote");
-let author = document.getElementById("author");
-let btn = document.getElementById("btn");
-
-const url = "https://api.quotable.io/random";
-
-let getQuote = () => {
-    fetch(url)
-        .then((data) => data.json())
-        .then((item) => {
-            quote.innerText = item.content;
-            author.innerText = item.author;
-        });
-};
-window.addEventListener("load", getQuote);
-btn.addEventListener("click", getQuote);
-
-
 let marker;
 let data;
 async function checkWeather(city) {
